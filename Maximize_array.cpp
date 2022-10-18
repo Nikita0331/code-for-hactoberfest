529752be5be28e2a089ce020c77f34f772744a1f
Srijan235 <98459863+Srijan235@users.noreply.github.com>
2022-10-18 19:40:52 +0530

    Update Maximize_array.cpp

--- Maximize_array.cpp
+++ Maximize_array.cpp
@@ -3,27 +3,49 @@
 using namespace std; 
   
 
+
 class Solution{
     public:
     int maximizeSum(int a[], int n) 
     {
         // Complete the function
-        unordered_map<int, int> m;
-        int sum = 0;
-        for (int i = 0; i < n; i++)
-        {
+        unordered_map<int,int> m;
+        int i=n-1,sum=0;
+        for(int i=0;i<n;i++)
             m[a[i]]++;
-        }
-        for (int i = n - 1; i >= 0; i--)
+        
+        while(i>=0)
         {
-            if (m[a[i]] > 0)
+            int num=a[i];
+            i--;
+            
+            if(m.find(num)!=m.end())
             {
-                sum += a[i];
-                m[a[i]]--;
-                if (m[a[i] - 1] > 0)
-                    m[a[i] - 1]--;
+                sum+=num;
+                m[num]--;
+                
+                if(m[num]==0)
+                {
+                    auto it=m.find(num);
+                    m.erase(it);
+                }
+                
+            if(m.find(num-1)!=m.end())
+            {
+                m[num-1]--;
+                 if(m[num-1]==0)
+                {
+                    auto it=m.find(num-1);
+                    m.erase(it);
+                }
             }
+            
+            }
+            
+          
         }
+        
+        return sum;
     }
 
 };
@@ -54,5 +76,3 @@ int main()
     }
 
 }
-
-
