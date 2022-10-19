8c4d3459ab317329c9c6ba09cbe62215bac3b96b
Vidushi Pandey <86524341+Vidushi7555@users.noreply.github.com>
2022-10-20 00:03:21 +0530

    Create fun.java

---
+++ fun.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ 
+class GFG {
+    static int fun(int i)
+    {
+        if (i % 2 == 1) return (i++);
+        else return fun(fun(i - 1));
+    }
+     
+    public static void main (String[] args) {
+        System.out.println(" " + fun(200) + " ");
+    }
+}
+ 
